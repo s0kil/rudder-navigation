c5624010b0d5476485a2ef95c0f30a6a1b21ff13
Daniel Sokil <dtechtours@gmail.com>
2020-10-16 20:43:48 -0400

    Animation Working

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
-const container = document.querySelector('.container');
-const plusBtn = document.getElementById('plus');
+const container = document.querySelector(".container");
+const plusBtn = document.getElementById("plus");
 
-plusBtn.addEventListener('click', () => {
-  container.classList.toggle('expand');
-  container.classList.toggle('collapse');
-});
+const playTime = getComputedStyle(document.body).getPropertyValue(
+  "--play-time"
+);
+
+const a = playTime.match(/[0-9]/);
+
+const animatedElements = [".menu", "#button2", "#button1", "#plus"].map((e) =>
+  document.querySelector(e)
+);
+
+function toggleElementsAnimation(elements, timeToToggle) {
+  elements.forEach((e) => {
+    e.style.animationPlayState = "running";
+  });
+
+  setTimeout(() => {
+    elements.forEach((e) => {
+      e.style.animationPlayState = "paused";
+    });
+  }, timeToToggle);
+}
+
+plusBtn.addEventListener("click", () =>
+  toggleElementsAnimation(animatedElements, 1000)
+);
